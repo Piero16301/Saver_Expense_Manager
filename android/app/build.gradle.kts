ab0f97f91ce25aa9ac85663d74872f13ec2893a8
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2026-01-16 16:59:10 -0500

    Bump app version and update dependencies
    Increased Android app versionCode to 17 and versionName to 2.2.1. Updated google_fonts, hugeicons, and rive dependencies in pubspec.yaml to their latest patch versions.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.pmorales.saver.expense.manager"
         minSdk = 28
         targetSdk = 36
-        versionCode = 16
-        versionName = "2.2.0"
+        versionCode = 17
+        versionName = "2.2.1"
 
         // Configuración para tamaño de página de 16KB
         ndk {
